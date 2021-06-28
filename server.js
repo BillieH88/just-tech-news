0c368e8059fb415d3252643003bc20a688319cd7
Billie  Hussein <billiehussein@Billies-MacBook-Pro.local>
2021-06-27 22:18:30 -0400

    fixed env file issue

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequilize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+require('dotenv').config();
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 app.listen(PORT, () => {
+  console.log(process.env.DB_NAME)
+  console.log(process.env.DB_PW)
+  console.log(process.env.DB_USER)
   console.log('Now listening')
   sequelize.sync({force : false});
 });
